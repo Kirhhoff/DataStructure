01bbce997e95b5047b3812581375018df5ec46f9
Kirhhoff <1070280566@qq.com>
2018-10-12 13:02:45 +0800

    Add my previous implementation of Link-based List.

--- Stack/Main.cpp
+++ Stack/Main.cpp
@@ -1,6 +1,26 @@
 #include"AStack.h"
 #include<string.h>
 #include<stdio.h>
+
+long fact(int n){
+	Assert(n>=0,"n must be positive!\n");
+	AStack<int> stack(n);
+	stack.push(1);
+	stack.push(1);
+	for(int k=2;k<n;k++){
+		int v1=stack.pop();
+		int v2=stack.pop();
+		stack.push(v2);
+		stack.push(v1);
+		stack.push(v1+v2);
+	}
+	while(stack.length())
+		printf("%d ",stack.pop());
+}
+
+long factWithoutStack(int n){
+	
+}
 int main(){
 	AStack<char>* CS=new AStack<char>(40);
 	char word[]="I am here waiting for U.";
@@ -10,8 +30,8 @@ int main(){
 	while(--ptr>=0)
 		printf("%c\n%d\n%c\n",CS->pop(),CS->length(),CS->topValue());
 	delete CS;
-
-
+	
+	fact(15);
 
 
 
